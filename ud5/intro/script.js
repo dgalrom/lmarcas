a524caa0d069170c02003d1fbc1694715260b8da
dgalrom <dgalrom707@g.educaand.es>
2025-02-20 11:27:20 +0100

    Ejercicio Pares Impares

---
+++ ud5/intro/script.js
@@ -0,0 +1,27 @@
+let entrada = prompt("Ingrese números separados por espacios:", "");
+let pares = "";
+let impares = "";
+let noNumeros = "";
+
+if (entrada != null) {
+    let valores = entrada.split(" ");
+    
+    for (let i = 0; i < valores.length; i++) {
+        let valor = valores[i];
+        let num = Number(valor);
+        
+        if (valor == num && num != NaN) {
+            if (num % 2 == 0) {
+                pares = pares + num + ", ";
+            } else {
+                impares = impares + num + ", ";
+            }
+        } else {
+            noNumeros = noNumeros + valor + ", ";
+        }
+    }
+    
+    document.getElementById("pares").textContent = pares;
+    document.getElementById("impares").textContent = impares;
+    document.getElementById("no-numeros").textContent = noNumeros;
+}
